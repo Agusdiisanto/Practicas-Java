64ffe1c6e6dd241ded0848d078bd8104aa781f89
Agustin Di Santo <103080933+Agusdiisanto@users.noreply.github.com>
2022-09-14 20:20:56 -0300

    Delete HelloWorld directory

--- HelloWorld/src/MiNombre.java
+++
@@ -1,6 +0,0 @@
-public class MiNombre {
-    public static void main(String[] args) {
-        String miNombre = "Agustin" ;
-        System.out.println(miNombre);
-    }
-}
--- HelloWorld/src/PrimitivosBoolean.java
+++
@@ -1,9 +0,0 @@
-public class PrimitivosBoolean {
-    public static void main(String[] args) {
-        boolean datoLogico = true;
-        System.out.println("datoLogico = " + datoLogico);
-
-        boolean esIgual = (3-2 == 1);
-        System.out.println("esIgual = " + esIgual);
-    }
-}
